8eeb248c8377d9c199fc350195d6c2700b2a3a99
matthewmjm <matthewmjm@yahoo.com>
2021-04-22 20:23:58 -0600

    create room return

--- frontend/src/components/CreateRoomPage.js
+++ frontend/src/components/CreateRoomPage.js
@@ -11,11 +11,28 @@ import RadioGroup from "@material-ui/core/RadioGroup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 export default class CreateRoomPage extends Component {
+	defaultVotes = 2;
+
 	constructor(props) {
 		super(props);
 	}
 
 	render() {
-		return <p>This is the CREATE ROOM PAGE</p>;
+		return (
+			<Grid container spacing={1}>
+				<Grid item xs={12} align="center">
+					<Typography component="h4" variant="h4">
+						Create A Room
+					</Typography>
+				</Grid>
+				<Grid item xs={12} align="center">
+					<FormControl component="fieldset">
+						<FormHelperText>
+							<div align="center">Guest Control of Playback State</div>
+						</FormHelperText>
+					</FormControl>
+				</Grid>
+			</Grid>
+		);
 	}
 }
