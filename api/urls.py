bf676bc8663f73b99e6ff8831d261bc5c8f281c4
matthewmjm <matthewmjm@yahoo.com>
2021-04-26 20:38:19 -0600

    add getroom and join room to url patterns array

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,8 @@ from .views import RoomView, CreateRoomView;
 
 urlpatterns = [
     path('room', RoomView.as_view()),
-    path('create-room', CreateRoomView.as_view())
+    path('create-room', CreateRoomView.as_view()),
+    path('get-room', GetRoom.as_view()),
+    path('join-room', JoinRoom.as_view())
 
 ]
