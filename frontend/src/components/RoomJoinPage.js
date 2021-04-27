002a8801f3749b3d5dc2d142f257e1b435085645
matthewmjm <matthewmjm@yahoo.com>
2021-04-26 21:09:05 -0600

    CSS for center

--- frontend/src/components/RoomJoinPage.js
+++ frontend/src/components/RoomJoinPage.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
-
+import { TextField, Button, Grid, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
 export default class RoomJoinPage extends Component {
-  constructor(props) {
-    super(props);
-  }
+	constructor(props) {
+		super(props);
+	}
 
-  render() {
-    return <p>This is the JOIN ROOM PAGE</p>;
-  }
+	render() {
+		return <p>This is the JOIN ROOM PAGE</p>;
+	}
 }
